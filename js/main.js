fdd4a26d624ffb39bbe4e1b51171c4d4092ea654
komal.jayswal <komal.jayswal@maersk.com>
2024-07-22 20:50:12 +0530

    refactor

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// main.js for Home Page functionality
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Home Page Loaded');
-    // Add any additional JavaScript functionality for the Home Page here
-});
--- js/shop.js
+++
@@ -1,6 +0,0 @@
-// shop.js for Shop Page functionality
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Shop Page Loaded');
-    // Add any additional JavaScript functionality for the Shop Page here
-});
